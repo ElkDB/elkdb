c444825fce78b185d99532d7e95c494a6b8afe9d
Malik Naik <maliknaik16@gmail.com>
2021-04-27 11:44:34 -0400

    Update Makefile, main.c, sql.l, and sql.y

--- src/main.c
+++ src/main.c
@@ -1,11 +1,31 @@
 
-#include "sql.tab.c"
-#include "lex.yy.c"
-#include "custom_reader.h"
-
 #include <readline/readline.h>
 #include <readline/history.h>
 #include <string.h>
+#include <vector>
+#include <iostream>
+#include <fstream>
+
+#include "utilities.c"
+#include "outputmanager.cpp"
+#include "datamanager.cpp"
+
+using namespace DataManager;
+using namespace ElkOutput;
+
+// Initialize the pointers to zero.
+DataManager::BucketManager* BucketManager::s_instance = 0;
+OutputManager* OutputManager::s_instance = 0;
+
+// Get instances of the Manager objects.
+OutputManager* output_manager = OutputManager::getInstance();
+DataManager::BucketManager* bucket_manager = BucketManager::getInstance();
+
+#include "datahandler.cpp"
+
+#include "sql.tab.c"
+#include "lex.yy.c"
+#include "custom_reader.h"
 
 static char* query = (char*) NULL;
 
@@ -19,7 +39,7 @@ char* get_query()
     }
 
     // Get the query.
-    query = readline ("> ");
+    query = readline ("elkdb> ");
 
     // Add the query to the history.
     if (query && *query) {
@@ -28,13 +48,113 @@ char* get_query()
 
   return query;
 }
-int main(void) {
-    char* q;
-    q = get_query();
 
-    while(strlen(q) >= 5 && strncmp(q, "\\exit", 5) != 0) {
-        parse_query(q);
+void printHelp()
+{
+    std::cout << "Usage: " << std::endl;
+    std::cout << "\t" << "elkdb [options]" << std::endl;
+    std::cout << std::endl;
+    std::cout << "options:" << std::endl;
+    std::cout << "\t" << "-h --help" << "\t" << "Print the help documentation" << std::endl;
+    std::cout << "\t" << "-f --file FILE" << "\t" << "Execute the commands from the FILE" << std::endl;
+    std::cout << std::endl;
+}
 
-        q = get_query();
+void parseArguments(int argc, char* argv[])
+{
+    bool file = false;
+    bool continue_execution = true;
+    std::string filename;
+
+    if(argc >= 2)
+    {
+        continue_execution = false;
+        for(int i = 1; i < argc; i++)
+        {
+            if(file)
+            {
+                filename = std::string(argv[i]);
+                break;
+            }
+
+            if(strcmp(argv[i], "-h") == 0 || strcmp(argv[i], "--help") == 0)
+            {
+                printHelp();
+                break;
+            }
+
+            if(strcmp(argv[i], "-f") == 0 || strcmp(argv[i], "--file") == 0)
+            {
+                file = true;
+            }
+        }
+
+        if(file)
+        {
+            std::string line;
+            std::fstream infile(filename);
+
+            while(std::getline(infile, line))
+            {
+                // std::cout << line << std::endl;
+                // if(strncmp(line.c_str(), "\\exit", 5))
+                // {
+                //     break;
+                // }
+
+                if(strcmp(line.c_str(), "") == 0 || strcmp(line.c_str(), "\n") == 0 || strcmp(line.c_str(), "\t") == 0) {
+                    continue;
+                }
+                parse_query(line.c_str());
+            }
+
+            infile.close();
+            continue_execution = true;
+        }
     }
+
+    if(continue_execution)
+    {
+        char* q;
+        q = get_query();
+
+        while(strncmp(q, "\\exit", 5) != 0) {
+            parse_query(q);
+
+            q = get_query();
+        }
+    }
+}
+
+int main(int argc, char* argv[])
+{
+    std::cout << "  ______   _   _      _____    ____  " << std::endl;
+    std::cout << " |  ____| | | | |    |  __ \\  |  _ \\ " << std::endl;
+    std::cout << " | |__    | | | | __ | |  | | | |_) |" << std::endl;
+    std::cout << " |  __|   | | | |/ / | |  | | |  _ < " << std::endl;
+    std::cout << " | |____  | | |   <  | |__| | | |_) |" << std::endl;
+    std::cout << " |______| |_| |_|\\_\\ |_____/  |____/ " << std::endl;
+    std::cout << "" << std::endl;
+    std::cout << "ElkDB Version 1.0.0 alpha (April 27, 2021)" << std::endl;
+    std::cout << std::endl;
+
+    parseArguments(argc, argv);
+
+    // std::string document_string = "{\"CarName\":\"Sonata\",\"CarType\":\"Gasoline\",\"CarMaker\":\"Hyundai\",\"CarInCode\":\"000001\",\"CarPart\":{\"PubPartName\":\"Battery\",\"PopPartName\":\"Battery\",\"PartType\":\"Null\",\"PartNum\":1,\"PartWeight\":{\"PartFeWt\":\"0kg\",\"PartNonFeWt\":\"11.44kg\",\"PartPIWt\":\"4.40kg\",\"PartGumWt\":\"0kg\",\"PartGlassWt\":\"0kg\",\"PartSpongeWt\":\"0kg\",\"PartLiquidWt\":\"0kg\",\"PartWasteWt\":\"6.16kg\",\"TotalWt\":\"22kg\"},\"RecycleWt\":\"15.84kg\",\"RecycleRate\":\"72.0%\",\"ClassType1\":\"Basic\",\"ClassType2\":\"Basic Dismantling\",\"PartMark\":\"Good condition\"},\"Station\":\"10\",\"CarInform\":{\"Transmission\":\"Auto\",\"displacement\":\"1820CC\",\"CarPhoto\":\"Null\",\"InDate\":\"2015-01-04T00:00:00Z\",\"EndDate\":\"2015-01-08T00:00:00Z\",\"MoveYN\":\"8Y\",\"MoveKM\":\"53,000KM\",\"Colors\":\"White\",\"RecycleYN\":\"Y\",\"Weight\":\"1920kg\"},\"WorkerName\":\"Joohyoung\",\"CarMark\":{\"Note\":\"Good condition but front bumper damaged, defective transmissions. By Worker 00001 Joohyoung\"}}";
+
+    // bucket_manager->createBucket("cars");
+    // bucket_manager->insertIntoBucket("cars", "car_1", document_string);
+
+    // create_bucket("scraped_results");
+    // create_bucket("deduped_results");
+
+    // insert_into_bucket("deduped_results", "1", "{\"hello\": \"world\"}");
+    // insert_into_bucket("scraped_results", "45", "{\"bonjour\": \"monde\"}");
+    // insert_into_bucket("scraped_results", "25", "{\"nihao\": \"world\"}");
+    // insert_into_bucket("scraped_results", "30", "{\"hola\": \"test\"}");
+
+    // insert into cars values ('car_1', '{"CarName":"Sonata","CarType":"Gasoline","CarMaker":"Hyundai","CarInCode":"000001","CarPart":{"PubPartName":"Battery","PopPartName":"Battery","PartType":"Null","PartNum":1,"PartWeight":{"PartFeWt":"0kg","PartNonFeWt":"11.44kg","PartPIWt":"4.40kg","PartGumWt":"0kg","PartGlassWt":"0kg","PartSpongeWt":"0kg","PartLiquidWt":"0kg","PartWasteWt":"6.16kg","TotalWt":"22kg"},"RecycleWt":"15.84kg","RecycleRate":"72.0%","ClassType1":"Basic","ClassType2":"Basic Dismantling","PartMark":"Good condition"},"Station":"10","CarInform":{"Transmission":"Auto","displacement":"1820CC","CarPhoto":"Null","InDate":"2015-01-04T00:00:00Z","EndDate":"2015-01-08T00:00:00Z","MoveYN":"8Y","MoveKM":"53,000KM","Colors":"White","RecycleYN":"Y","Weight":"1920kg"},"WorkerName":"Joohyoung","CarMark":{"Note":"Good condition but front bumper damaged, defective transmissions. By Worker 00001 Joohyoung"}}');
+
+    return 0;
+
 }
--- src/sql.y
+++ src/sql.y
@@ -11,7 +11,7 @@ void yyerror(const char* s);
 
 %}
 
-%start  query
+%start  _start
 
 %union  {
     int intval;
@@ -24,6 +24,7 @@ void yyerror(const char* s);
 %token  UPDATE
 %token  UPSERT
 %token  CREATE
+%token  BUCKET
 %token  FROM
 %token  INTO
 %token  VALUES
@@ -47,6 +48,7 @@ void yyerror(const char* s);
 %token  DIV
 %token  BOOLEAN
 
+%token  FORWARD_SLASH
 %token  SEMICOLON
 %token  COLON
 %token  COMMA
@@ -55,53 +57,96 @@ void yyerror(const char* s);
 %token  LSBRACE
 %token  RSBRACE
 
+%token  SHOW_BUCKETS
+%token  SET_INDENT
+
 %token  INTEGER
 %token  REAL
 %token  STR
 
-%type <strval> name attributeList VARIABLE_NAME STR MUL
+%type <strval> name keys VARIABLE_NAME STR MUL
 %type <intval> INTEGER SEMICOLON WHITESPACE
 %type <realval> REAL
+
+// %destructor { printf("freed\n"); free($$); } <strval>
 
 %define parse.error verbose
 
 %%
 
+_start:
+      special_commands
+    | query
+    ;
+
+special_commands:
+      FORWARD_SLASH SHOW_BUCKETS
+      {
+        bucket_manager->listBuckets();
+        /*view_all_buckets();*/
+      }
+    |
+      FORWARD_SLASH SHOW_BUCKETS error
+      { }
+    | FORWARD_SLASH SET_INDENT INTEGER
+      {
+        output_manager->setIndent((unsigned)$3);
+      }
+    | FORWARD_SLASH SET_INDENT INTEGER error
+      {}
+    ;
+
 query:
-        statement SEMICOLON
-        { printf("Found Statement.\n"); }
-        ;
+      statement SEMICOLON
+      { }
+    ;
 
 statement:
-          SELECT attributeList  FROM name
-          { printf("Found SELECT '%s' FROM '%s';\n", $2, $4); }
-        | INSERT INTO name VALUES LPAREN STR COMMA STR RPAREN
-          { printf("Found INSERT '%s' FROM ('%s', '%s');\n", $3, $6, $8); }
-        | UPDATE name SET name EQ STR
-          { printf("Found UPDATE '%s' FROM '%s' = '%s'", $2, $4, $6); }
-        | UPDATE name SET name EQ REAL
-          { printf("Found UPDATE '%s' FROM '%s' = '%f'", $2, $4, $6); }
-        | UPDATE name SET name EQ INTEGER
-          { printf("Found UPDATE '%s' FROM '%s' = '%d'", $2, $4, $6); }
-        ;
+      SELECT keys FROM name
+      {
+        /* printf("Found SELECT '%s' FROM '%s';\n", $2, $4); */
+        //select_bucket($2, $4);
+        bucket_manager->selectBucket($2, $4);
+      }
+    |
+      CREATE BUCKET name
+      {
+        bucket_manager->createBucket($3);
+      }
+    |
+      INSERT INTO name VALUES LPAREN STR COMMA STR RPAREN
+      {
+        bucket_manager->insertIntoBucket($3, $6, $8);
+      }
+    |
+      UPDATE name SET name EQ STR
+      { printf("Found UPDATE '%s' FROM '%s' = '%s'", $2, $4, $6); }
+    |
+      UPDATE name SET name EQ REAL
+      { printf("Found UPDATE '%s' FROM '%s' = '%f'", $2, $4, $6); }
+    |
+      UPDATE name SET name EQ INTEGER
+      { printf("Found UPDATE '%s' FROM '%s' = '%d'", $2, $4, $6); }
+    ;
 
-attributeList:
-          name AS name
-          { $$ = strcat(strcat($1, ":"), $3); }
-        | name ',' attributeList
-          { $$ = strcat(strcat($1, ","), $3); }
-        | name AS name ',' attributeList
-          { $$ = strcat(strcat($1, ","), $3); }
-        | MUL
-          { $$ = $1; }
-        | name
-          { $$ = $1; }
-        ;
+keys:
+      name AS name
+      { $$ = strcat(strcat($1, ":"), $3); }
+    |
+      name ',' keys
+      { $$ = strcat(strcat($1, ","), $3); }
+    |
+      MUL
+      { $$ = $1; }
+    |
+      name
+      { $$ = $1; }
+    ;
 
 name:
-          VARIABLE_NAME
-          { $$ = $1; }
-          ;
+      VARIABLE_NAME
+      { $$ = $1; }
+    ;
 
 %%
 
